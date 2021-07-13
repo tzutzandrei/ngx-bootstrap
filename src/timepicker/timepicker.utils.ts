6d962fbbfa86e098ff17bb195984214fb53156f6
Andrei Tut <andrei@tooktech.com>
2021-07-13 08:07:44 +0300

    Fix time picker validation when using min max

--- src/timepicker/timepicker.utils.ts
+++ src/timepicker/timepicker.utils.ts
@@ -189,16 +189,16 @@ export function isSecondInputValid(seconds: string): boolean {
 }
 
 export function isInputLimitValid(diff: Time, max?: Date, min?: Date): boolean {
-  const newDate = setTime(new Date(), diff);
+  let newDate = setTime(new Date(), diff);
 
   if (!newDate) {
     return false;
   }
-
+  newDate.setFullYear(max.getFullYear(), max.getMonth(), max.getDate());	
   if (max && newDate > max) {
     return false;
   }
-
+  newDate.setFullYear(min.getFullYear(), min.getMonth(), min.getDate());	
   if (min && newDate < min) {
     return false;
   }
